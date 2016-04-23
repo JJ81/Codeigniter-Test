855ce864bff53511390b6e230cf99634deee4bf8
yijaejun <yijaejun81@gmail.com>
2016-04-23 19:42:01 +0900

    no message

--- application/config/config.php
+++ application/config/config.php
@@ -36,7 +36,7 @@ $config['base_url'] = 'http://localhost/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = ''; // index.php를 지운다.
 
 /*
 |--------------------------------------------------------------------------
